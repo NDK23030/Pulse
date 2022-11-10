0c52e0d26f8973fb65ecb16a8fd16d9b9490b58e
Denis <ggmazanovdenis@gmail.com>
2022-11-10 16:25:37 +0300

    optimization settings

--- src/js/script.js
+++ src/js/script.js
@@ -100,24 +100,24 @@ $(document).ready(function(){
 
       new WOW().init();
 
-    //   $('form').submit(function(e) {
-    //     e.preventDefault();
+      $('form').submit(function(e) {
+        e.preventDefault();
 
-    //     if (!$(this).valid()) {
-    //         return;
-    //     }
+        if (!$(this).valid()) {
+            return;
+        }
 
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "mailer/smart.php",
-    //         data: $(this).serialize()
-    //     }).done(function() {
-    //         $(this).find("input").val("");
-    //         $('#consultation, #order').fadeOut();
-    //         $('.overlay, #thanks').fadeIn('slow');
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
 
-    //         $('form').trigger('reset');
-    //     });
-    //     return false;
-    //   });
+            $('form').trigger('reset');
+        });
+        return false;
+      });
   });
